c96dabe265f9b29dff06d034c4f44e2b3a097244
marwameskine <maroumes12@hotmail.com>
2026-03-17 11:13:21 -0400

    milestone for our healthcare scheduling system

---
+++ src/main/java/com/champsoft/healthcaremilestone/HealthcareMilestoneApplication.java
@@ -0,0 +1,13 @@
+package com.champsoft.healthcaremilestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareMilestoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareMilestoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/champsoft/healthcaremilestone/HealthcareMilestoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.champsoft.healthcaremilestone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareMilestoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
