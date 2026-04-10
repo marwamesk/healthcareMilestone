9226e17e5348e17cfb1a400e7d23c5eec5efcf92
marwamesk <maroumes12@hotmail.com>
2026-04-10 11:15:11 -0400

    okay

---
+++ src/main/java/com/champsoft/healthcaremilestone/modules/doctor/domain/exception/DoctorNotFoundException.java
@@ -0,0 +1,8 @@
+package com.champsoft.healthcaremilestone.modules.doctor.domain.exception;
+
+public class DoctorNotFoundException extends RuntimeException{
+    public DoctorNotFoundException(String message) {
+        super(message);
+    }
+}
+
